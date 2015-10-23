faec42970801c138ce971b4e100bdcb8e1d5fedc
voltan <djvoltan@gmail.com>
2015-10-23 12:47:12 +0330

    Work on admin side

---
+++ src/Controller/Admin/SendController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+/**
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+namespace Module\Message\Controller\Admin;
+
+use Pi;
+use Pi\Mvc\Controller\ActionController;
+use Pi\Paginator\Paginator;
+use Module\Guide\Form\CustomerForm;
+use Module\Guide\Form\CustomerFilter;
+
+class SendController extends ActionController
+{
+    public function indexAction()
+    {
+    }
+}
