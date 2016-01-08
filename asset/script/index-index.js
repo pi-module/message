2b1a57988d4666b3c8bbc9c23c23a4aefe04cf0a
voltan <djvoltan@gmail.com>
2016-01-08 17:18:52 +0330

    Work on message module

--- asset/script/index-index.js
+++ asset/script/index-index.js
@@ -12,14 +12,14 @@
             this.$el = $('#message-js');
             this.$delete = this.$('a[data-confirm]');
             this.$select = this.$('.message-batch-action');
-            // this.$items = this.$('.message-item');
+            this.$items = this.$('.message-item');
             this.$batch = this.$('.message-js-batch');
             this.$confirm = this.$('.confirm-ok');
         },
         bindEvents: function () {
             this.$batch.click(this.checkedAll);
             this.$select.change($.proxy(this.batchAction, this));
-            // this.$items.bind('click', this.itemsBind);
+            this.$items.bind('click', this.itemsBind);
             this.$delete.click(this.deleteAction);
             this.$confirm.on('click', $.proxy(this.confirmAction, this));
         },
